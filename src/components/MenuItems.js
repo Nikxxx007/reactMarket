55cc6e636174041dae91bb1448c70b3ec405144d
Nick <nikita.n007@mail.ru>
2022-05-12 23:59:44 +0300

    styling MenuItem

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -2,15 +2,46 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ItemsArea = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  
+  @media (max-width: 990px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const StyledItem = styled.div`
-  max-height: 200px;
+  display: flex;
+  min-height: 200px;
   border: solid 1px gray;
   border-radius: 6px;
-  margin: 0 5px;
+  margin: 0 10px 20px;
+  padding: 25px;
+`
+
+const StyledImage = styled.img`
+  height: 120px;
+  border-radius: 6px;
+  margin: auto 25px auto 0;
+`
+
+const Flex = styled.div`
+  display: flex;
+  margin: ${props => props.margin};
+  justify-content: ${props => props.justifyContent};
+`
+
+const Block = styled.div`
+  display: block;
+  flex: 1;
+`
+
+const ItemHeader = styled.h6`
+  color: 08090a;
+  font-size: 20px;
+  line-height: 1;
+  font-weight: 700;
+  letter-spacing: -0.03em;
 `
 
 function MenuItems( {items} ) {
@@ -19,11 +50,22 @@ function MenuItems( {items} ) {
             <ItemsArea>
                 {
                     items.map((items) => {
-                        const { id, title, category, price, image, description } = items;
+                        const {id, title, category, price, image, description} = items;
                         return (
                             <StyledItem>
-                                <h2>{title}</h2>
-                                <p>{description} {price}</p>
+                                <StyledImage src={image}/>
+                                <Block>
+                                    <Flex justifyContent={'space-between'} margin={'10px 0'} >
+                                        <ItemHeader>{title}</ItemHeader>
+                                        <p color={'#35b8be'}>$ {price} USD</p>
+                                    </Flex>
+                                    <p>{description}</p>
+                                    <Flex>
+                                        <input type={'number'} min={1} max={10}/>
+                                        <button>Add To Cart</button>
+                                    </Flex>
+                                </Block>
+
                             </StyledItem>
                         );
                     })
