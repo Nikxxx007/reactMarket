2793c4dcb046319386e2207aa08c68a9713dfe4b
Nick <nikita.n007@mail.ru>
2022-05-26 20:59:10 +0300

    starting cart functionality

--- src/components/MenuData.js
+++ src/components/MenuData.js
@@ -3,7 +3,7 @@ const menu = [
         id: 1,
         title: 'Burger Dreams',
         category: 'Burgers',
-        price: 9.20,
+        price: '9.20',
         image: 'images/foodImages/5e865e09d8efa3676276b5cd_Burger 03.png',
         description: 'Random text'
     },
@@ -11,7 +11,7 @@ const menu = [
         id: 2,
         title: 'Burger Waldo',
         category: 'Burgers',
-        price: 10.00,
+        price: '10.00',
         image: 'images/foodImages/5e865e09d8efa3b2e676b5b5_Burger 08.png',
         description: 'Random text2'
     },
@@ -19,7 +19,7 @@ const menu = [
         id: 3,
         title: 'Burger Cali',
         category: 'Burgers',
-        price: 12.00,
+        price: '12.00',
         image: 'images/foodImages/5e865e09d8efa36d8576b5b4_Burger 04.png',
         description: 'Random text2'
     },
@@ -27,7 +27,7 @@ const menu = [
         id: 4,
         title: 'Salad Ceaser',
         category: 'Slides',
-        price: 6.00,
+        price: '6.00',
         image: 'images/foodImages/5e865e09d8efa30d7276b5ac_Side 06.png',
         description: 'Random text3'
     },
@@ -35,7 +35,7 @@ const menu = [
         id: 5,
         title: 'Beans Slowed Cooked',
         category: 'Slides',
-        price: 4.00,
+        price: '4.00',
         image: 'images/foodImages/5e865e09d8efa360e276b609_Side 05.png',
         description: 'Random text4'
     },
@@ -43,7 +43,7 @@ const menu = [
         id: 6,
         title: 'Drink Fig&Lime',
         category: 'Drinks',
-        price: 4.00,
+        price: '4.00',
         image: 'images/foodImages/5e865e09d8efa3409976b5b0_Drink 1.png',
         description: 'Random text5'
     },
@@ -51,7 +51,7 @@ const menu = [
         id: 7,
         title: 'Drink Liquor',
         category: 'Drinks',
-        price: 7.00,
+        price: '7.00',
         image: 'images/foodImages/5e865e09d8efa30dc176b5af_Drink 2.png',
         description: 'Random text6'
     }
--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -27,13 +27,18 @@ const StyledImage = styled.img`
 
 const Flex = styled.div`
   display: flex;
+  height: ${props => props.height};
+  margin: ${props => props.margin};
+  flex-grow: ${props => props.flex_grow};
   margin: ${props => props.margin};
   justify-content: ${props => props.justifyContent};
+  align-items: ${props => props.alignItems};
 `
 
 const Block = styled.div`
   display: block;
   flex: 1;
+  position: relative;
 `
 
 const ItemHeader = styled.h6`
@@ -42,6 +47,35 @@ const ItemHeader = styled.h6`
   line-height: 1;
   font-weight: 700;
   letter-spacing: -0.03em;
+`
+
+const CartDiv = styled.div`
+  position: absolute;
+  bottom: 0;
+`
+
+const CartButton = styled.button`
+  min-height: 45px;
+  justify-content: center;
+  border-radius: 6px;
+  background: #35b8bc;
+  padding: 9px 15px;
+  border: 0;
+  color: white;
+`
+
+const StyledInput = styled.input`
+  type: number;
+  defaultValue: 1;
+  min: 1;
+  max: 10;
+  padding: 8px 6px 8px 12px;
+  margin-right: 8px;
+  line-height: 20px;
+  border-radius: 6px;
+  background-color: #fafafa;
+  border: 1px solid #ddd;
+  min-height: 45px;
 `
 
 function MenuItems( {items} ) {
@@ -60,12 +94,11 @@ function MenuItems( {items} ) {
                                         <p color={'#35b8be'}>$ {price} USD</p>
                                     </Flex>
                                     <p>{description}</p>
-                                    <Flex>
-                                        <input type={'number'} min={1} max={10}/>
-                                        <button>Add To Cart</button>
-                                    </Flex>
+                                    <CartDiv>
+                                        <StyledInput type={'number'} defaultValue={1} min={1} max={10}/>
+                                        <CartButton onClick={}>Add To Cart</CartButton>
+                                    </CartDiv>
                                 </Block>
-
                             </StyledItem>
                         );
                     })
